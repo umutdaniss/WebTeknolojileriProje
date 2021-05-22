7261ad34d22d30fbb47671db705e8668fd926a10
umutdaniss <umut.danis@ogr.sakarya.edu.tr>
2021-05-22 23:52:08 +0300

    22.05.2021

---
+++ 22.05.2021/4. kommit/baglanti.php
@@ -0,0 +1,19 @@
+<?php
+$vt_sunucu="localhost";
+$vt_kullanici="root";
+$vt_sifre="";
+$vt_adi="website";
+
+$baglan=mysqli_connect($vt_sunucu, $vt_kullanici, $vt_sifre, $vt_adi);
+
+if(!$baglan){
+    
+    die("Bağlantı başarısız".mysqli_connect_error());
+}
+
+
+
+
+
+
+?>
